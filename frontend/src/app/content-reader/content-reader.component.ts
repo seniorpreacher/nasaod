fae33a6593ed7a066b250bdd2acb51bee09999fe
Daniel Salamon <salidani@gmail.com>
2019-03-03 13:40:04 +0100

    Frontend - initial large-screen design

--- frontend/src/app/content-reader/content-reader.component.ts
+++ frontend/src/app/content-reader/content-reader.component.ts
@@ -7,10 +7,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ContentReaderComponent implements OnInit {
 
+  paragraphs: { id: number, content: string }[] = [];
+
   constructor() {
   }
 
   ngOnInit() {
+    const exampleText = 'Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ' +
+      'ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable ' +
+      'cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn ' +
+      'with silver.';
+
+    for (let i = 0; i < 100; i++) {
+      this.paragraphs.push({id: i, content: exampleText});
+    }
   }
 
 }
