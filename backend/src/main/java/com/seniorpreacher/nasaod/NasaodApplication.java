f3582d59be0529af857f3e4d24b36adea14fc83d
Daniel Salamon <salidani@gmail.com>
2019-03-06 10:31:58 +0100

    Backend - Enable CORS

--- backend/src/main/java/com/seniorpreacher/nasaod/NasaodApplication.java
+++ backend/src/main/java/com/seniorpreacher/nasaod/NasaodApplication.java
@@ -4,6 +4,7 @@ import com.seniorpreacher.nasaod.paragraphService.ParagraphService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +22,10 @@ public class NasaodApplication {
         SpringApplication.run(NasaodApplication.class, args);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping("/")
-    public List paragraphRoute(@RequestParam(name = "offset") int offset, @RequestParam(name = "limit") int limit) {
-        List responseParagraphs = new ArrayList<ParagraphResponse>();
+    public List<ParagraphResponse> paragraphRoute(@RequestParam(name = "offset") int offset, @RequestParam(name = "limit") int limit) {
+        List<ParagraphResponse> responseParagraphs = new ArrayList<>();
         for (int i = offset; i < offset + limit; i++) {
             responseParagraphs.add(new ParagraphResponse(i, this.paragraphService.getParagraph()));
         }
